1e475c53779ea24cbc8576bbbef2715d3dd74594
Magdalena Castillo Alemany <Malena@MacBook-Air-de-Magdalena.local>
2020-07-21 20:19:20 +0200

    Fixing css

--- docs/menu.js
+++ docs/menu.js
@@ -1,6 +1,6 @@
 function renderButtons() {
   document.querySelectorAll('.btn').forEach(function(element) {
-    element.style.width = element.clientHeight + "px"
+    element.style.height = element.clientWidth + "px"
   });
 
 }
