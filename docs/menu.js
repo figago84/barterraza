cd39a80fe6d4821f0923707fc0d25e895ef37c1b
Magdalena Castillo Alemany <Malena@MacBook-Air-de-Magdalena.local>
2020-07-21 20:02:26 +0200

    adding buttons to the bottom

--- docs/menu.js
+++ docs/menu.js
@@ -4,3 +4,15 @@ function renderButtons() {
   });
 
 }
+
+function showMenu(menuName){
+  menu = document.getElementsByClassName(menuName)[0];
+  children = menu.children;
+  length = menu.children.length;
+  for (i=1; i < length; i++){
+    if (children[i].children[0].style.visibility == "hidden" || children[i].children[0].style.visibility == '')
+      children[i].children[0].style.visibility = "visible";
+    else 
+      children[i].children[0].style.visibility = "hidden";
+  }
+}
