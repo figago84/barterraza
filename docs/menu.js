7608ea4a09115a4d0f9c00286541e687ffd89a6f
Magdalena Castillo Alemany <Malena@MacBook-Air-de-Magdalena.local>
2020-07-22 21:19:59 +0200

    Menu fix

--- docs/menu.js
+++ docs/menu.js
@@ -1,26 +1,33 @@
 function renderButtons() {
-  document.querySelectorAll('.btn').forEach(function(element) {
+  document.querySelectorAll('.btn').forEach(function (element) {
     element.style.height = element.clientWidth + "px"
-    if (!element.className.includes("selected"))
-      element.style.display = "none"
+    // if (!element.className.includes("selected"))
+    //   element.style.display = "none"
   });
 
+  document.getElementsByClassName("top non-selected")[0].style.display = "none"
+  document.getElementsByClassName("bottom non-selected")[0].style.display = "none"
+
 }
 
-function showMenu(menuName){
-  menu = document.getElementsByClassName(menuName)[0];
-  children = menu.children;
-  length = menu.children.length;
-  for (i=1; i < length; i++){
+function showMenu(menuName) {
 
-     if (children[i].children[0].style.display == "none" || children[i].children[0].style.display == '')
-       children[i].children[0].style.display = "flex";
-     else 
-       children[i].children[0].style.display = "none";
-  }
+  menu = document.getElementsByClassName(menuName + " non-selected")[0];
+  if (menu.style.display == "none" || menu.style.display == '')
+    menu.style.display = "block";
+  else
+    menu.style.display = "none";
+  // children = menu.children;
+  // length = menu.children.length;
+  // for (i=1; i < length; i++){
+
+  //    if (children[i].children[0].style.display == "none" || children[i].children[0].style.display == '')
+  //      children[i].children[0].style.display = "flex";
+  //    else 
+  //      children[i].children[0].style.display = "none";
+  // }
 }
 
-function goTo(site)
-{
-  location.href="https://figago84.github.io/barterraza/" + site + ".html"
+function goTo(site) {
+  location.href = "https://figago84.github.io/barterraza/" + site + ".html"
 }
